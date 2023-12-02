57c5953105a7bcd18b660b4eca2eb20d773c812c
ysmt <enuswi67@gmail.com>
2023-12-02 17:45:52 +0900

    feat: hot reload & cloud run

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,10 @@
-////import { Link, User } from "./db";
-//import { Link } from "./db/link";
-//
-//const database = new Link();
-//
-////database.create("test", "https://yahoo.com");
-////const user = new User();
-////user.create();
-//
-//const link = new Link();
-//link.create("test", "https://yahoo.com");
-//
-//const run = async () => {
-//  const list = await database.getList();
-//  list.map(item => console.log(item));
-//};
-//
-//run();
-
-
 import express from 'express';
 const app: express.Express = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//CROS対応（というか完全無防備：本番環境ではだめ絶対）
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
-
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Start on port 3000.");
 });
 
